5e90b6dd166f010c3f04b560e38410b3518f8d59
Dana Nobile <danagnobile@gmail.com>
2020-08-12 12:58:36 -0400

    Added functions to move images

--- podcast-click-game/src/App.js
+++ podcast-click-game/src/App.js
@@ -1,45 +1,83 @@
 import React, { Component } from "react";
 import PodcastCard from "./components/PodcastCard/podcast";
 import Wrapper from "./components/Wrapper/wrapper";
-import Title from "./components/Title/title";
 
+import Score from "./components/Score/score";
 import podcasts from "./podcasts.json";
-
+import "./App.css";
 
 
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
-  state = { podcasts };
+  state = {
+    podcasts,
+    clickedPodcasts: [],
+    score: 0,
+    goal: 12,
+    status: ""
 
-
-
-
-
-
-  MovePodcast = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const podcasts = this.state.podcasts.filter(podcast => podcast.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ podcasts });
   };
 
+  shuffleScoreCard = id => {
+    let clickedPodcasts = this.state.clickedPodcasts;
+
+    if (clickedPodcasts.includes(id)) {
+      this.setState({ cclickedPodcasts: [], score: 0, status: "Game Over! Sorry, you lost. Click to play again!" });
+      return;
+    } else {
+      clickedPodcasts.push(id)
+
+      if (clickedPodcasts.length === 12) {
+        this.setState({ score: 12, status: "You Won! Great Job, True Crime Junkie! Click to play again!", clickedPodcasts: [] });
+        console.log('You Win');
+        return;
+      }
+
+      this.setState({ podcasts, clickedPodcasts, score: clickedPodcasts.length, status: " " });
+
+      for (let i = podcasts.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [podcasts[i], podcasts[j]] = [podcasts[j], podcasts[i]];
+      }
+    }
+  }
+
+
+  // Map over this.state.cards and render a Card component for each card object
   render() {
     return (
-
-      <Wrapper>
-        <Title>My Favorite True Crime Podcasts</Title>
-        {this.state.podcasts.map(podcast => (
-          <PodcastCard
-            removeFriend={this.removeFriend}
-            id={podcast.id}
-            image={podcast.image}
-
-          />
-        ))}
-      </Wrapper>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Podcast Clicky Game</h1>
+          <p className="App-intro">
+            Your goal is to try and click all of the podcasts, without clicking the same image twice! Look out though! The images start to move on you, so flex your memory muscle!
+        </p>
+        </header>
+        <Score total={this.state.score}
+          goal={12}
+          status={this.state.status}
+        />
+        <Wrapper>
+          {this.state.podcasts.map(podcastID => (
+            <PodcastCard
+              shuffleScoreCard={this.shuffleScoreCard}
+              id={podcastID.id}
+              key={podcastID.id}
+              image={podcastID.image}
+            />
+          ))}
+        </Wrapper>
+        <footer>
+          <p>Designed and built by Dana Nobile.</p>
+        </footer>
+      </div>
     );
   }
+
+
+
+
 }
 
 
